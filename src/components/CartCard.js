7a8c1c339c41cde985f5cbfa01b6a525ac8888ca
Yatharth Shah <yatharhshah00@gmail.com>
2024-01-04 03:21:33 -0500

    first commit

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,6 +1,8 @@
+import { useCart } from "../context/CartContext";
 import "./CartCard.css"
 
 export const CartCard = ({product}) => {
+  const {removefromCart} = useCart();
   const {name, price, image} = product;
 
   return (
@@ -8,7 +10,7 @@ export const CartCard = ({product}) => {
       <img src={image} alt="" />
       <p className="productName">{name}</p>
       <p className="productPrice">${price}</p>
-      <button>Remove</button>
+      <button onClick={()=>removefromCart(product)}>Remove</button>
     </div>
   )
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,23 +1,18 @@
-import { useTitle } from "../hooks/useTitle"
-import { CartCard } from "../components"
+import { useCart } from "../context/CartContext";
+import { useTitle } from "../hooks/useTitle";
+import { CartCard } from "../components";
+
 export const Cart = () => {
-  useTitle("Cart")
+  const { total, cartList } = useCart();
+  useTitle("Cart");
 
-  
-
-  const products = [
-    {"id": 5, "name": "APPLE Airpods Max Bluetooth Headset", "price": 199, "image": "/assets/images/1005.png"},
-    {"id": 6, "name": "ZEBRONICS Zeb-Thunder Wired", "price": 29, "image": "/assets/images/1006.png"}
-
-  ]
   return (
     <main>
       <section className="cart">
-        <h1>Cart Items: {products.length}</h1>
-        {products.map((product)=>(
-          <CartCard key={product.id} product={product}/>
-        ))}
-        
+        <h1>Cart Items: {cartList.length} / ${total}</h1>
+        { cartList.map((product) => (
+          <CartCard key={product.id} product={product} />
+        )) }        
       </section>
     </main>
   )
